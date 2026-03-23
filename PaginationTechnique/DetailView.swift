d9b53b962d81f894ebe80acf18cee2907b1d6e1c
Swayambhu Jyoti Banerjee <swayambhu.2010@gmail.com>
2026-03-23 14:20:28 +0530

    Detail screen added

---
+++ PaginationTechnique/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  PaginationTechnique
+//
+//  Created by Swayambhu BANERJEE on 23/03/26.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
